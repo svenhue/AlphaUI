7a1aa17f4858a139739bef56ce354e463e14deee
Sven Hüning <sven-huening@web.de>
2024-01-02 13:09:02 +0100

    unnötiges löschen

--- examples/react-full/renderer/+onHydrationEnd.ts
+++
@@ -1,8 +0,0 @@
-// https://vike.dev/onHydrationEnd
-export { onHydrationEnd }
-
-import type { OnHydrationEndAsync } from 'vike/types'
-
-const onHydrationEnd: OnHydrationEndAsync = async (): ReturnType<OnHydrationEndAsync> => {
-  console.log('Hydration finished; page is now interactive.')
-}
--- examples/vike-react/vite.config.ts
+++
@@ -1,8 +0,0 @@
-import react from '@vitejs/plugin-react'
-import vike from 'vike/plugin'
-import { UserConfig } from 'vite'
-
-export default {
-  plugins: [react(), vike()],
-  optimizeDeps: { include: ['cross-fetch', 'react/jsx-runtime'] }
-} satisfies UserConfig
